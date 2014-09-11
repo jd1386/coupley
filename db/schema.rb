2a41ce86d9db682948eaf16174f72a5bc3305d4f
Jungdo Lee <lee.jungdo@gmail.com>
2014-09-11 21:23:37 +0900

    add first name and last name to user

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20140911073242) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140911122149) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -13,6 +26,8 @@ ActiveRecord::Schema.define(version: 20140911073242) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
