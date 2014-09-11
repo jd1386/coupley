50444ab24ca990e6be1c276b6df19ce60a80c20e
Jungdo Lee <lee.jungdo@gmail.com>
2014-09-11 23:10:33 +0900

    add more style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Initial config as suggested by Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 end
