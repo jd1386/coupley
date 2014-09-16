a07735b82de7854cbd31d96211ce67dc44e0dbef
Jungdo Lee <lee.jungdo@gmail.com>
2014-09-16 11:19:15 +0900

    add title to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915060639) do
+ActiveRecord::Schema.define(version: 20140916015611) do
 
   create_table "posts", force: true do |t|
     t.text     "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140915060639) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "title"
   end
 
   add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
